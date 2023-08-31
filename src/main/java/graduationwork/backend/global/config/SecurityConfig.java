ffa4255bcea7cd74abd3f8b9aa9c3687d5d6bf5b
djc06048 <gsafe1213@gmail.com>
2023-09-01 01:16:05 +0900

    [feat] 스프링시큐리티 설정파일 세팅

---
+++ src/main/java/graduationwork/backend/global/config/SecurityConfig.java
@@ -0,0 +1,119 @@
+package graduationwork.backend.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import graduationwork.backend.domain.user.repository.UserRepository;
+import graduationwork.backend.global.config.jwt.JwtAuthenticationEntryPoint;
+import graduationwork.backend.global.config.jwt.filter.JwtAuthenticationProcessingFilter;
+import graduationwork.backend.global.config.jwt.filter.JwtExceptionFilter;
+import graduationwork.backend.global.config.jwt.service.JwtService;
+import graduationwork.backend.global.login.filter.CustomJsonUsernamePasswordAuthenticationFilter;
+import graduationwork.backend.global.login.handler.LoginFailureHandler;
+import graduationwork.backend.global.login.handler.LoginSuccessHandler;
+import graduationwork.backend.global.login.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig {
+    private final LoginService loginService;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+
+    private final JwtAuthenticationEntryPoint entryPoint;
+    private final JwtExceptionFilter jwtExceptionFilter;
+    private static final List<RequestMatcher> specialUrlMatchers = List.of(
+            new AntPathRequestMatcher("/api/v1/user/login/**"),
+            new AntPathRequestMatcher("/api/v1/user/signup/**"),
+            new AntPathRequestMatcher("/swagger-ui/**"),
+            new AntPathRequestMatcher("/v3/api-docs/**")
+
+    );
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(specialUrlMatchers.toArray(new RequestMatcher[0])).permitAll()
+                .anyRequest().authenticated();
+
+
+        http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
+        http.addFilterBefore(jwtAuthenticationProcessingFilter(), CustomJsonUsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtExceptionFilter, jwtAuthenticationProcessingFilter().getClass());
+        http.addFilterBefore(jwtAuthenticationProcessingFilter(), BasicAuthenticationFilter.class)
+                .exceptionHandling(handler -> handler.authenticationEntryPoint(entryPoint))
+        ;
+
+        return http.build();
+
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
+        return new JwtAuthenticationProcessingFilter(jwtService, userRepository,specialUrlMatchers);
+    }
+
+    @Bean
+    public CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter() {
+        CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordLoginFilter
+                = new CustomJsonUsernamePasswordAuthenticationFilter(objectMapper);
+        customJsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+        customJsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
+        customJsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return customJsonUsernamePasswordLoginFilter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(loginService);
+        return new ProviderManager(provider);
+    }
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler() {
+        return new LoginSuccessHandler(jwtService, userRepository);
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
+    }
+
+}
+
+
+
+
+
+
