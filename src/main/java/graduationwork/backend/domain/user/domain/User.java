f4165ea47ecf82b9ed6538a4513898ab96e88a10
djc06048 <gsafe1213@gmail.com>
2023-09-01 01:20:12 +0900

    [feat] User 엔티티 추가

---
+++ src/main/java/graduationwork/backend/domain/user/domain/User.java
@@ -0,0 +1,70 @@
+package graduationwork.backend.domain.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+
+    private Float minCalories;
+    private Float minCarbs;
+    private Float minFat;
+    private Float minProtein;
+
+    private Float maxCalories;
+    private Float maxCarbs;
+    private Float maxFat;
+    private Float maxProtein;
+
+    private String profileImg;
+    private String refreshToken;
+
+
+
+
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
+    }
+
+
+    @Builder
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public void updateMinNutrients(Float minCalories, Float minCarbs, Float minFat, Float minProtein) {
+        this.minCalories = minCalories;
+        this.minCarbs = minCarbs;
+        this.minFat = minFat;
+        this.minProtein = minProtein;
+
+    }
+    public void updateMaxNutrients(Float maxCalories, Float maxCarbs, Float maxFat, Float maxProtein) {
+        this.maxCalories = maxCalories;
+        this.maxCarbs = maxCarbs;
+        this.maxFat = maxFat;
+        this.maxProtein = maxProtein;
+
+    }
+}
