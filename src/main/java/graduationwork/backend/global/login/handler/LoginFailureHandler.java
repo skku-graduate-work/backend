88030d3535d40735cea489949ce37706f46081e8
djc06048 <gsafe1213@gmail.com>
2023-09-01 01:11:16 +0900

    [feat] 자체 로그인 구현

---
+++ src/main/java/graduationwork/backend/global/login/handler/LoginFailureHandler.java
@@ -0,0 +1,24 @@
+package graduationwork.backend.global.login.handler;
+
+import graduationwork.backend.global.error.exception.ErrorCode;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/plain;charset=UTF-8");
+        response.getWriter().write(ErrorCode.NOT_MATCH_LOGIN_INFORMATION.getMessage());
+        log.info("로그인에 실패했습니다. 메시지 : {}", exception.getMessage());
+    }
+}
---
+++ src/main/java/graduationwork/backend/global/login/service/LoginService.java
@@ -0,0 +1,31 @@
+package graduationwork.backend.global.login.service;
+
+
+import graduationwork.backend.domain.user.domain.User;
+import graduationwork.backend.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class LoginService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
