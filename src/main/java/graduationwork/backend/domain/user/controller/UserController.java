8c7caa1a8fbfaa43e23b060f965b96798613c189
djc06048 <gsafe1213@gmail.com>
2023-09-19 18:14:24 +0900

    [feat] #7 #8 식재료 가져오기, 이미지 수정 컨트롤러 추가

--- src/main/java/graduationwork/backend/domain/user/controller/UserController.java
+++ src/main/java/graduationwork/backend/domain/user/controller/UserController.java
@@ -4,6 +4,7 @@ import graduationwork.backend.domain.user.domain.User;
 import graduationwork.backend.domain.user.repository.UserRepository;
 import graduationwork.backend.domain.user.dto.NutrientsRequestDto;
 import graduationwork.backend.domain.user.dto.SignUpRequestDto;
+import graduationwork.backend.global.S3Service;
 import graduationwork.backend.global.error.exception.ErrorCode;
 import graduationwork.backend.global.error.exception.UnauthorizedException;
 import graduationwork.backend.domain.user.service.UserService;
@@ -11,14 +12,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -28,15 +29,27 @@ import java.util.Optional;
 public class UserController {
     private final UserService userService;
     private final UserRepository userRepository;
-
     @PostMapping("/signup")
     @Operation(summary = "회원가입")
     public ResponseEntity SignUp(@RequestBody SignUpRequestDto signUpRequestDto) {
         return userService.join(signUpRequestDto);
     }
 
+    @PostMapping(value="/update-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "프로필 이미지 변경", description = "s3 와 연동되어 프로필 이미지를 변경 가능")
+    public ResponseEntity updateProfileImage(@RequestParam(value="image") MultipartFile file,Authentication authentication) throws IOException {
+        log.info("이미지 변경");
+        String email = authentication.getName();
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if (user.isPresent()) {
+            return userService.updateProfileImage(file, user.get());
+        } else throw new UnauthorizedException(ErrorCode.UNAUTHORIZED);
+
+
+    }
     @PutMapping("/min-nutrients")
-    @Operation(summary = "개인 영양소 하한선 설정",description = "칼로리, 탄수화물, 지방, 단백질 하한선 설정하기")
+    @Operation(summary = "개인 영양소 하한선 설정", description = "칼로리, 탄수화물, 지방, 단백질 하한선 설정하기")
     public ResponseEntity SetMinNutrients(@RequestBody NutrientsRequestDto nutrientsRequestDto, Authentication authentication) {
         String email = authentication.getName();
         Optional<User> user = userRepository.findByEmail(email);
@@ -48,7 +61,7 @@ public class UserController {
     }
 
     @PutMapping("/max-nutrients")
-    @Operation(summary = "개인 영양소 상한선 설정",description = "칼로리, 탄수화물, 지방, 단백질 상한선 설정하기")
+    @Operation(summary = "개인 영양소 상한선 설정", description = "칼로리, 탄수화물, 지방, 단백질 상한선 설정하기")
     public ResponseEntity SetMaxNutrients(@RequestBody NutrientsRequestDto nutrientsRequestDto, Authentication authentication) {
         String email = authentication.getName();
         Optional<User> user = userRepository.findByEmail(email);
@@ -60,4 +73,16 @@ public class UserController {
     }
 
 
+    @GetMapping("/ingredient")
+    @Operation(summary = "사용자 기본정보와 영양정보, 냉장고 속 재료 가져오기", description = "사용자 기본정보와 영양정보, 냉장고 속 재료 가져오기")
+    public ResponseEntity GetUserAndIngredient(Authentication authentication) {
+        log.info("/ingredient");
+        String email = authentication.getName();
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if (user.isPresent()) {
+            log.info(user.get().getEmail());
+            return userService.getUserAndIngredient(user.get());
+        } else throw new UnauthorizedException(ErrorCode.UNAUTHORIZED);
+    }
 }
