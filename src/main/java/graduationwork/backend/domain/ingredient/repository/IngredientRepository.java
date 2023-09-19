813b8825b7b634d548b40682013141a66e03a76c
djc06048 <gsafe1213@gmail.com>
2023-09-19 18:11:16 +0900

    [feat] #8 user 의 ingredient 가져오는 쿼리 추가

--- src/main/java/graduationwork/backend/domain/ingredient/repository/IngredientRepository.java
+++ src/main/java/graduationwork/backend/domain/ingredient/repository/IngredientRepository.java
@@ -6,7 +6,9 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IngredientRepository extends JpaRepository<Ingredient,Long> {
@@ -16,4 +18,7 @@ public interface IngredientRepository extends JpaRepository<Ingredient,Long> {
 
     @Query("select i from Ingredient i where i.name_ko=:name")
     Optional<Ingredient> findIngredientByNameKo(@Param("name") String name);
+
+    @Query("select i from Ingredient i where i.user.id=:id")
+    List<Ingredient> findIngredientsByUser(@Param("id") Long userId);
 }
