a767f99010ec81b9703ecc99daf0dd2e54231cea
HyeLim_.p <62535887+djc06048@users.noreply.github.com>
2023-09-22 20:46:58 +0900

    [chore] deploy.sh 수정
    기존 포트 있으면 죽이고 새 포트 열 수 있게 최종 수정 완료

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,10 @@
 REPOSITORY=/home/ubuntu/app/step2
 cd $REPOSITORY
 
-APPNAME=java
 JAR_NAME=$(ls $REPOSITORY/zip | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/zip/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(lsof -t -i:8080)
 echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z $CURRENT_PID ]
