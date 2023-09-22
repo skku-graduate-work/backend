7c2d45abf9c1af25848fe29bf4536dd0e090baa6
HyeLim_.p <62535887+djc06048@users.noreply.github.com>
2023-09-22 20:47:34 +0900

    [chore] #19 deploy.sh 수정완료

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,5 +24,5 @@ echo ">JAR Path: $JAR_PATH"
 echo ">JAR_PATH 에 실행권한 추가"
 chmod +x $JAR_PATH
 
-echo "> Deploy - $JAR_PATH "
+echo "> Deploy - $JAR_PATH"
 nohup java -jar -Dspring.config.location=classpath:/application.yml,/home/ubuntu/app/application-oauth.yml,/home/ubuntu/app/application-prod.yml,/home/ubuntu/app/application-aws.yml,/home/ubuntu/app/application-api.yml,/home/ubuntu/app/application-jwt.yml $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
