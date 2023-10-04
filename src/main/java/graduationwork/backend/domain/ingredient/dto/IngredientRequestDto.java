e16604e3673d8340f027f8659c56414d117ada59
djc06048 <gsafe1213@gmail.com>
2023-10-04 20:58:07 +0900

    [fix] #20 식재료 등록 json dto 수정

--- src/main/java/graduationwork/backend/domain/ingredient/dto/IngredientRequestDto.java
+++ src/main/java/graduationwork/backend/domain/ingredient/dto/IngredientRequestDto.java
@@ -10,7 +10,6 @@ import java.time.LocalDate;
 public class IngredientRequestDto {
     @NotNull(message = "재료 이름이 입력되지 않았습니다")
     private String name;
-    private  String image;
     private  String expiration_date;
     private  Float calories;
     private Float carbs;
@@ -18,9 +17,8 @@ public class IngredientRequestDto {
     private Float protein;
 
     @Builder
-    public IngredientRequestDto(String name, String image, String expiration_date, Float calories, Float carbs, Float fat, Float protein) {
+    public IngredientRequestDto(String name, String expiration_date, Float calories, Float carbs, Float fat, Float protein) {
         this.name = name;
-        this.image = image;
         this.expiration_date = expiration_date;
         this.calories = calories;
         this.carbs = carbs;
